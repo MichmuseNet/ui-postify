9a651e2799f75d25c96833e6a961f924ac406ef3
Andrea Michelle Olivas Rivera <andreaolivas@Andreas-MacBook-Pro-3.local>
2026-04-21 23:01:44 -0600

    update1

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="bg-sky-300">
+      <h1 className="text-3xl md:bg-sky-300 font-bold underline">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App/>} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         </Routes>
     </BrowserRouter>
   </StrictMode>,
